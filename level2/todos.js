0f6c53bb03604760361abbf7fe013e95ea8f7ccb
EnekoDev <enekolabdev@gmail.com>
2026-03-03 12:32:45 +0100

    working on todo functions

--- level2/todos.js
+++ level2/todos.js
@@ -1,3 +1,17 @@
+const fs = require("node:fs/promises")
 const express = require("express");
 const app = express();
 
+const FILE = "./todos.json";
+
+export async function getTodos() {
+    const data = await readJson(FILE);
+    app.get("/todos", (req, res) => {
+        res.json(data);
+    });
+}
+
+async function readJson(file) {
+    const rawJson = await fs.readFile(file, { encoding: "utf-8" });
+    return JSON.parse(rawJson);
+}
