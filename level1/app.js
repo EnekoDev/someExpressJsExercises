4a99cc0db1ff073c969c8d668da4b65a804b5283
EnekoDev <enekolabdev@gmail.com>
2026-03-03 10:45:26 +0100

    working with widdlewares

--- level1/app.js
+++ level1/app.js
@@ -17,6 +17,40 @@ app.get('/contact', (req, res) => {
     res.send('Contact Page');
 });
 
+
+app.get('/users/:id', (req, res, next) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.status(404).json({
+            error: "ID is required"
+        });
+    }
+    res.send(`User ID is: ${id}`);
+});
+
+app.get('/posts/:postId/comments/:commentId', (req, res) => {
+    const { postId, commentId } = req.params;
+    if (!postId) {
+        return res.status(404).json({
+            error: "Post ID is required"
+        });
+    }
+    if (!commentId) {
+        return res.status(404).json({
+            error: "Comment ID is required"
+        });
+    }
+    res.send(`Comment ${commentId} in post ${postId}`);
+});
+
+app.get('/search', (req, res) => {
+    const { term, limit } = req.query;
+    res.json({
+        term: term,
+        limit: limit
+    });
+});
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
--- level2/app.js
+++ level2/app.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/users/:id', (req, res) => {
-    let id = req.params.id ?? 0;
-    res.send(`Welcome user ${id}`);
-});
+app.use((req, res, next) => {
+    const key = req.query.apiKey;
+    console.log(key)
+    if (!key || key !== "12345") {
+        return res.status(403).json({
+            Error: "Unauthorized"
+        });
+    }
+    req.requestTime = Date.now();
+    next();
+})
 
-app.get('/posts/:postId/comments/:commentId', (req, res) => {
-    let postId = req.params.postId ?? 0;
-    let commentId = postId !== 0 && (req.params.commentId ?? 0) ? req.params.commentId : 0;
-    res.send(`Comment ${commentId} in post ${postId}`);
-});
+app.get("/", (req, res) => {
+    const time = req.requestTime;
+    res.json({
+        timestamp: time
+    })
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
