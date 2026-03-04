893160a177059dffb1fd0368c1586faadb636781
EnekoDev <enekolabdev@gmail.com>
2026-03-04 16:12:03 +0100

    working on todo functions

--- level2/todos.js
+++ level2/todos.js
@@ -11,6 +11,25 @@ export async function getTodos() {
     });
 }
 
+export async function getSingleTodo() {
+    const data = await readJson(FILE);
+    app.get("/todos/:id", (req, res) => {
+        const { id } = req.params;
+        if (!id) {
+            res.status(400).json({
+                error:"ID is required"
+            });
+            return;
+        }
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id === id) {
+                res.json(data[i]);
+                return;
+            }
+        }
+    });
+}
+
 async function readJson(file) {
     const rawJson = await fs.readFile(file, { encoding: "utf-8" });
     return JSON.parse(rawJson);
