b725a334993acda557b2ef1fd2040601d8b348d7
EnekoDev <enekolabdev@gmail.com>
2026-03-16 16:42:26 +0100

    delete done and new response created

--- level3/usersCrud.js
+++ level3/usersCrud.js
@@ -3,9 +3,10 @@ import { connect } from "./connection.js";
 
 const response = {
     success: (res, data) => { res.status(200).json({ data: data }) },
+    failed: (res, err) => { res.status(400).json({ error: "The request has failed: " + err }) },
     badReq: (res, field) => { res.status(400).json({ error: `The ${field} is required` }) },
     notFound: (res) => { res.status(404).json({ error: "The element does not exist" }) },
-    serverError: (res, err) => { res.status(500).json({ error: "Server error: " + err }) },    
+    serverError: (res, err) => { res.status(500).json({ error: "Server error: " + err }) },
 }
 
 const con = await connect();
@@ -67,14 +68,32 @@ usersRouter.put("/users/:id", async (req, res) => {
 
         const user = db.findOne({ userId:numId });
 
-        if (name) user.name = name;
-        if (role) user.role = role;
+        const updateFields = {};
+        if (name) updateFields.name = name;
+        if (role) updateFields.role = role;
 
-        const update = db.updateOne({ userId:numId }, {
-            userId:numId,
-            name:name,
-            role:role
+        const update = await db.updateOne({ userId:numId }, {
+            $set: updateFields
         });
+
+        return response.success(res, "Update completed");
+    } catch (err) {
+        return response.serverError(res, err);
+    }
+});
+
+usersRouter.delete("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const numId = Number(id);
+        if (!Number.isInteger(numId) || numId < 1) return response.badReq(res);
+
+        const result = await db.deleteOne({ userId:numId });
+
+        if (result.deleteCount !== 1) {
+            return response.failed(res, result);
+        }
+        return response.success(res, result);
     } catch (err) {
         return response.serverError(res, err);
     }
